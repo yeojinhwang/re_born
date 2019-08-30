93c8f7867b93616e09c1292fddec260a7137f5e8
hwangyeojin <hwangyj0202@gmail.com>
2019-08-30 22:26:33 +0900

    login 페이지 구현

--- src/pages/sign/Sign.vue
+++ src/pages/sign/Sign.vue
@@ -1,230 +1,91 @@
 <template>
-  <v-container>
-    <v-layout>
-        <p>Re-born</p>
-    </v-layout>
-    <v-layout>
-      <!-- login provider -->
-        <div id="loginProvider">
-          <v-btn 
-            class="loginBtn"
-            depressed small style="color: #0F9D58;"
-            @click="googleLogin">
-            <v-icon right dark style="
-              margin-left: 0px;
-              margin-right: 7px;
-              ">fab fa-google</v-icon>
-            Google 로그인
-          </v-btn>
-        </div>
-      </v-layout>
-      <v-divider></v-divider>
-      <!-- email with password -->
-      <v-layout>
-      <v-form
-        ref="form"
-        v-model="form"
-        v-show="viewSign"
-        >
-        <!-- email -->
-        <v-text-field
-          v-model="email"
-          :rules="emailRules"
-          filled
-          label="Email address"
-          type="email"
-          style="width:240px; margin: auto;"
-        ></v-text-field>
-        <!-- displayName -->
-        <!-- 회원가입 폼에서만 보임 -->
-        <v-text-field
-          v-model="displayName"
-          :rules="nameRules"
-          filled
-          label="name"
-          name="nameForm"
-          style="width:240px; margin: auto;"
-          v-on:keyup="characterCheck()"
-          v-on:keydown="characterCheck()"
-        ></v-text-field>
-        <!-- password -->
-        <v-text-field
-          v-model="password"
-          :append-icon="pwShow ? 'visibility' : 'visibility_off'"
-          :rules="pwRules"
-          filled
-          label="Password"
-          :type="pwShow ? 'text' : 'password'"
-          @click:append="pwShow = !pwShow"
-          style="width:240px; margin: auto;"
-        ></v-text-field>
-
-        <v-btn
-          class="loginBtn"
-          :loading="loading"
-          :disabled="!form"
-          color="success"
-          @click="loader()"
-        >
-          가입하기
-        </v-btn>
-      </v-form>
-      
-      <v-form
-        ref="form"
-        v-model="form"
-        v-show="!viewSign"
-        >
-        <!-- email -->
-        <v-text-field
-          v-model="email"
-          :rules="emailRules"
-          filled
-          label="Email address"
-          type="email"
-          style="width:240px; margin: auto;"
-        ></v-text-field>
-        <!-- password -->
-        <v-text-field
-          v-model="password"
-          :append-icon="pwShow ? 'visibility' : 'visibility_off'"
-          :rules="pwRules"
-          filled
-          label="Password"
-          :type="pwShow ? 'text' : 'password'"
-          @click:append="pwShow = !pwShow"
-          style="width:240px; margin: auto;"
-        ></v-text-field>
-
-        <v-btn
-          class="loginBtn"
-          :loading="loading"
-          :disabled="!form"
-          color="success"
-          @click="loader()"
-        >
-          로그인
-        </v-btn>
-      </v-form>
-    </v-layout>
-  </v-container>
+  <div style="background-color: #DDEAD3; height: 100vh;">
+    <div style="margin-left: 35vw; padding-top: 30vh;">
+      <img class="animated tada slow infinite" style="height: 5rem; margin-left: 7vw;" src="https://firebasestorage.googleapis.com/v0/b/reborn-4c850.appspot.com/o/src%2Freborn-logo.png?alt=media&token=4396dd6c-df8a-494d-ba58-282ee02b3848"/>
+      <p style="font-size: 2.5rem; font-family: 'Poiret One', cursive; font-weight: bold; padding-top: 2vh;">Re-born</p>
+    </div>
+    <!-- login provider -->
+    <div id="loginProvider" style="margin-left: 26vw; padding-top: 7vh;">
+      <button type="button" class="google-button" @click="googleLogin">
+        <span class="google-button__icon">
+          <svg viewBox="0 0 366 372" xmlns="http://www.w3.org/2000/svg"><path d="M125.9 10.2c40.2-13.9 85.3-13.6 125.3 1.1 22.2 8.2 42.5 21 59.9 37.1-5.8 6.3-12.1 12.2-18.1 18.3l-34.2 34.2c-11.3-10.8-25.1-19-40.1-23.6-17.6-5.3-36.6-6.1-54.6-2.2-21 4.5-40.5 15.5-55.6 30.9-12.2 12.3-21.4 27.5-27 43.9-20.3-15.8-40.6-31.5-61-47.3 21.5-43 60.1-76.9 105.4-92.4z" id="Shape" fill="#EA4335"/><path d="M20.6 102.4c20.3 15.8 40.6 31.5 61 47.3-8 23.3-8 49.2 0 72.4-20.3 15.8-40.6 31.6-60.9 47.3C1.9 232.7-3.8 189.6 4.4 149.2c3.3-16.2 8.7-32 16.2-46.8z" id="Shape" fill="#FBBC05"/><path d="M361.7 151.1c5.8 32.7 4.5 66.8-4.7 98.8-8.5 29.3-24.6 56.5-47.1 77.2l-59.1-45.9c19.5-13.1 33.3-34.3 37.2-57.5H186.6c.1-24.2.1-48.4.1-72.6h175z" id="Shape" fill="#4285F4"/><path d="M81.4 222.2c7.8 22.9 22.8 43.2 42.6 57.1 12.4 8.7 26.6 14.9 41.4 17.9 14.6 3 29.7 2.6 44.4.1 14.6-2.6 28.7-7.9 41-16.2l59.1 45.9c-21.3 19.7-48 33.1-76.2 39.6-31.2 7.1-64.2 7.3-95.2-1-24.6-6.5-47.7-18.2-67.6-34.1-20.9-16.6-38.3-38-50.4-62 20.3-15.7 40.6-31.5 60.9-47.3z" fill="#34A853"/></svg>
+        </span>
+        <span class="google-button__text">Sign in with Google</span>
+      </button>
+    </div>
+  </div>
 </template>
 
 <script>
 import FirebaseServices from '../../services/FirebaseServices'
 import firebase from 'firebase'
 import router from '../../router'
-import store from '../../store'
 
 export default {
   name: 'Sign',
   data () {
     return {
-      viewSign: false,
-      // login modal
-      agreement: false,
-      dialog: false,
-      // form
-      form: false,
-      // input rules
-      email: '',
-      emailRules: [
-        v => !!(v || '').match(/@/) || '@를 포함한 email을 입력해주세요.',
-      ],
-      displayName: '',
-      nameRules: [
-        v => !!v || '이름을 입력해주세요.',
-      ],
-      // pw rule
-      pwShow: false,
-      password: '',
-      pwRules: [
-        v => !!v || '비밀번호를 입력해주세요.',
-        v => v.length >= 8 || '최소 8자 이상 입력해주세요.',
-      ],
-      // service rule
-      agreeRules: [
-        v => !!v || '동의가 필요합니다.',
-      ],
-      // loading button
-      loading: false,
+      user: null
     }
   },
+  mounted() {
+  },
   methods: {
-    loader() {
-      // btn 클릭에 맞는 func
-      if(this.viewSign == true) {
-        this.createUserWithEmail()
-      } else {
-        this.emailLogin()
-      }
-      // 로딩활성화
-      this.loading = true
-    },
-    async createUserWithEmail() {
-      let _this = this
-      await firebase.auth().createUserWithEmailAndPassword(this.email, this.password)
-        .then(function(user) {
-          FirebaseServices.createdbForNewUser(user.user.uid, _this.$store.state.today, _this.displayName)
-          // 유저 생성하면서 입력받은 이름 설정
-          let _user = firebase.auth().currentUser
-          _user.updateProfile({
-              displayName: _this.displayName,
-              photoURL: 'http://dy.gnch.or.kr/img/no-image.jpg'
-          })
-        //   setTimeout(function () {
-        //     router.push('/')
-        //   }, 2000)
-        })
-        .catch(function(err) {
-            if (err.code == 'auth/email-already-in-use') {
-              alert('이미 존재하는 이메일입니다. 다른 이메일을 사용해주세요.')
-              _this.email = ''
-            }
-            _this.loading = false
-            _this.agreement = false
-        })
-    },
-    async emailLogin() {
-      let _this = this
-      await firebase.auth().signInWithEmailAndPassword(this.email, this.password)
-        .then(function(result) {
-          store.commit('setPhotoURL', result.user.photoURL)
-          setTimeout(function () {
-            router.push('/')
-          }, 2000)
-        })
-        .catch(function(err) {
-          if (err.code == 'auth/user-not-found') {
-            alert('존재하지 않는 유저 입니다.')
-            _this.email = ''
-            _this.password = ''
-          }
-          else if (err.code == 'auth/wrong-password') {
-            alert ('비밀번호가 틀렸습니다. 다시 입력해주세요.')
-            _this.password = ''
-          }
-          _this.loading = false
-        })
-    },
     async googleLogin() {
-      await FirebaseServices.loginUserWithGoogle()
-      firebase.auth().onAuthStateChanged(function(user) {
-        if (user) {
-          router.push('/')
-        }
+      let tmp = await FirebaseServices.loginUserWithGoogle()
+      let temp = firebase.auth().onAuthStateChanged(function(user) {
+        console.log(user)
       })
-    },
-    // 유저네임 특수문자 제한
-    characterCheck() {
-      let RegExp = /[ \{\}\[\]\/?.,;:|\)*~`!^\-_+┼<>@\#$%&\'\"\\\(\=]/
-      let obj = document.getElementsByName("nameForm")[0]
-      if (RegExp.test(obj.value)) {
-        // 현재 데이터와 연결해서 특수문자 입력을 받으면 마지막 문자를 제거
-        this.displayName = obj.value.substring(0, obj.value.length - 1)
-        alert('특수문자는 입력하실 수 없습니다.')
-      }
     }
   }
 }
 </script>
+
+<style>
+  .google-button {
+    height: 40px;
+    border-width: 0;
+    background: white;
+    color: #737373;
+    border-radius: 5px;
+    white-space: nowrap;
+    box-shadow: 1px 1px 0px 1px rgba(0,0,0,0.05);
+    transition-property: background-color, box-shadow;
+    transition-duration: 150ms;
+    transition-timing-function: ease-in-out;
+    padding: 0;
+    
+    &:focus,
+    &:hover {
+      box-shadow: 1px 4px 5px 1px rgba(0,0,0,0.1);
+    }
+    
+    &:active {
+      background-color: #e5e5e5;
+      box-shadow: none;
+      transition-duration: 10ms;
+    }
+  }
+      
+  .google-button__icon {
+    display: inline-block;
+    vertical-align: middle;
+    margin: 8px 0 8px 8px;
+    width: 18px;
+    height: 18px;
+    box-sizing: border-box;
+  }
+
+  .google-button__icon--plus {
+    width: 27px;
+  }
+
+  .google-button__text {
+    display: inline-block;
+    vertical-align: middle;
+    padding: 0 24px;
+    font-size: 14px;
+    font-weight: bold;
+    font-family: 'Roboto',arial,sans-serif;
+  }
+</style>
