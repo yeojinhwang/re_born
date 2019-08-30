835e25f6b0a08f41b7d03a1fc301e49f9d029baf
unknown <aksenskf0k@gmail.com>
2019-08-31 01:45:40 +0900

    mypage

---
+++ src/pages/mypage/components/UserExp.vue
@@ -0,0 +1,39 @@
+<template>
+    <container>
+        <h1>{{userPoint}}</h1>
+        <v-btn>Button</v-btn>
+ 
+
+        <button @click="pointProgress">click</button>
+    </container>    
+</template>
+<script>
+export default {
+    name: 'UserExp',
+    data () {
+        return {
+            user : {
+                displayName: 'username',
+                points: 1700,
+                level: 'bronze',
+                photoURL: 'https://picsum.photos/id/11/300/300',
+                complaints : {
+                    1 : {
+                        writer : 'username',
+                        content : '환경을 위해 유색 페트병은 사용하지 말아주세요'
+                    }
+                }
+            },
+            userPoint : 10,
+            slider: 45
+        }
+    },
+    methods : {
+        pointProgress: function() {
+            this.userPoint += 10   
+        }
+    }
+    
+
+}
+</script>
---
+++ src/pages/mypage/components/UserInfo.vue
@@ -0,0 +1,43 @@
+<template>
+    <container>
+        <center>
+            <img 
+                :src="user.photoURL"
+                width="150"
+                height="150"
+                id="user_image_round"
+                style="margin-top: 10px"
+            />
+            <p class="text-align-center" style="margin: auto;font-size: 30px">{{user.displayName}}</p>
+        </center>
+    </container>
+
+</template>
+
+<script>
+export default {
+    name: 'MyPage',
+    data () {
+        return {
+            user : {
+                displayName: 'username',
+                points: 10,
+                level: 'bronze',
+                photoURL: 'https://picsum.photos/id/11/300/300',
+                complaints : {
+                    1 : {
+                        writer : 'username',
+                        content : '환경을 위해 유색 페트병은 사용하지 말아주세요'
+                    }
+                }
+            }
+        }
+    }
+    
+}
+</script>
+<style>
+    #user_image_round {
+        border-radius: 50%;
+    }
+</style>
