3c82401f97dd5485370641b584a53c1f847daee7
unknown <aksenskf0k@gmail.com>
2019-08-30 23:57:08 +0900

    file name change

---
+++ src/pages/mypage/MyPage.vue
@@ -0,0 +1,18 @@
+<template>
+    <container>
+        <UserInfo/>
+        <UserExp/>
+    </container>
+</template>
+<script>
+import UserInfo from './components/UserInfo.vue'
+import UserExp from './components/UserExp.vue'
+
+export default {
+    name: 'MyPage',
+    components: {
+        UserInfo,
+        UserExp
+    }
+}
+</script>
