a5a386de52a96f855c1fc6f73fdc456c522bf8d3
hwangyeojin <hwangyj0202@gmail.com>
2019-08-31 00:17:50 +0900

    debugging

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,30 +1,37 @@
 <template>
     <div>
         <v-toolbar
-        absolute
-        color="teal lighten-3"
-        dark
-        scroll-off-screen
-        scroll-target="#scrolling-techniques"
+            absolute
+            color="#B3D996"
+            dark
+            scroll-off-screen
+            scroll-target="#scrolling-techniques"
         >
-        <v-toolbar-side-icon></v-toolbar-side-icon>
-
-        <v-toolbar-title>Title</v-toolbar-title>
-
-        <v-spacer></v-spacer>
-
-        <v-btn icon>
-            <v-icon>search</v-icon>
-        </v-btn>
-
-        <v-btn icon>
-            <v-icon>favorite</v-icon>
-        </v-btn>
-
-        <v-btn icon>
-            <v-icon>more_vert</v-icon>
-        </v-btn>
+            <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+            <v-toolbar-title style="font-family: 'Poiret One', cursive; font-weight: bolder;">Re-born</v-toolbar-title>
         </v-toolbar>
+        <v-navigation-drawer
+            color="#B3D996"
+            v-model="drawer"
+            absolute
+            temporary
+        >
+        <v-list class="pt-0" dense>
+            <v-list-tile
+            v-for="item in items"
+            :key="item.title"
+            @click=""
+            >
+            <v-list-tile-action>
+                <v-icon>{{ item.icon }}</v-icon>
+            </v-list-tile-action>
+
+            <v-list-tile-content>
+                <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+            </v-list-tile-content>
+            </v-list-tile>
+        </v-list>
+        </v-navigation-drawer>
     </div>
 </template>
 
@@ -36,10 +43,14 @@ export default {
   name: 'Header',
   data () {
     return {
-      user: null
+        user: null,
+        drawer: false,
+        items: [
+            { title: 'Dashboard', icon: 'dashboard' },
+            { title: 'Account', icon: 'account_box' },
+            { title: 'Admin', icon: 'gavel' }
+        ]
     }
-  },
-  mounted() {
   },
   methods: {
   }
