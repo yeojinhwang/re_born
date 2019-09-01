bb87e941065411d72a203f62b810753e20d8c500
JeongTaeYeon <jty950315@gmail.com>
2019-09-01 11:42:06 +0900

    1

---
+++ frontend/dist/precache-manifest.2c217af3ca9495a80382df1ea19202c7.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "8644bb04c2bb1f4ef9fbe371b0a7910c",
+    "url": "/reborn-logo.png"
+  },
+  {
+    "revision": "2beaad0eb1b9df577f8f",
+    "url": "/css/app.b5de26cd.css"
+  },
+  {
+    "revision": "695f85e9aee5857d5aea",
+    "url": "/css/chunk-vendors.f1d18b77.css"
+  },
+  {
+    "revision": "695f85e9aee5857d5aea",
+    "url": "/js/chunk-vendors.1af40a10.js"
+  },
+  {
+    "revision": "1208afcc49f1bdc60cd8e9595645c6a3",
+    "url": "/img/camera_button.1208afcc.png"
+  },
+  {
+    "revision": "8644bb04c2bb1f4ef9fbe371b0a7910c",
+    "url": "/img/reborn-logo.8644bb04.png"
+  },
+  {
+    "revision": "b826b6c6281ab4ceef9170a2591a56b4",
+    "url": "/img/recycle.b826b6c6.png"
+  },
+  {
+    "revision": "9a8fb03af110df34254995072e6d2d7b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2beaad0eb1b9df577f8f",
+    "url": "/js/app.49fa6529.js"
+  },
+  {
+    "revision": "8cdb6bd6836d78a5d2206b561f8ca06d",
+    "url": "/tree_01.png"
+  },
+  {
+    "revision": "6bd422718f181d6fff3a097622d22835",
+    "url": "/tree_02.png"
+  },
+  {
+    "revision": "89651d6a39f07757c2611dc84fced08d",
+    "url": "/tree_03.png"
+  },
+  {
+    "revision": "ccfd5cd576734442336ce6fa5a4a82b7",
+    "url": "/tree_04.png"
+  },
+  {
+    "revision": "b5b20f27c86c0286370d937f849c3e45",
+    "url": "/tree_05.png"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "21a4c34dedc2b86baf539f39c4bedfe3",
+    "url": "/app_icon.png"
+  },
+  {
+    "revision": "a4e2271d19eb1f6f93a15e1b7a4e74dd",
+    "url": "/404.html"
+  }
+];
---
+++ frontend/src/main.js
@@ -0,0 +1,24 @@
+import Vue from "vue";
+import Vuetify from "vuetify";
+import App from "./App.vue";
+import store from "./store";
+import router from "./router";
+import "./registerServiceWorker";
+import GAuth from "vue-google-oauth2";
+
+
+Vue.config.productionTip = false;
+
+Vue.use(Vuetify);
+const gauthOption = {
+  clientId:
+    "25718941998-tp9dr97snqieperbnockj6elg2r2mr7n.apps.googleusercontent.com",
+  scope: "profile email",
+  prompt: "select_account"
+};
+Vue.use(GAuth, gauthOption);
+new Vue({
+  store,
+  router,
+  render: h => h(App)
+}).$mount("#app");
